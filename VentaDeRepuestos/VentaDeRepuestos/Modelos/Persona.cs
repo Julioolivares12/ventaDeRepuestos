21341ba00c1345b49951f9d03e2a54305275ca19
julio <julioolivares90@gmail.com>
2019-10-11 20:22:14 -0600

    agregadas clases modelo para persona y usuario

---
+++ VentaDeRepuestos/VentaDeRepuestos/Modelos/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaDeRepuestos.Modelos
+{
+   public class Persona
+    {
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string FechaNac{ get; set; }
+        public string Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+       
+
+
+    }
+}
---
+++ VentaDeRepuestos/VentaDeRepuestos/Modelos/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaDeRepuestos.Modelos
+{
+   public class Usuario : Persona
+    {
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
